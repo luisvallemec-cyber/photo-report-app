cd75d37b3ae1b4fdc2c7bbb99908bf2f5bdf9628
luisvallemec-cyber <luis.valle.mec@gmail.com>
2026-03-07 09:37:09 -0600

    Add DrawingPoint model with x, y coordinates and paint properties

---
+++ lib/models/drawing_model.dart
@@ -0,0 +1,8 @@
+class DrawingPoint {
+  final double x;
+  final double y;
+  final String color;
+  final double strokeWidth;
+
+  DrawingPoint({required this.x, required this.y, required this.color, required this.strokeWidth});
+}
